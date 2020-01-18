f6dc21d0f45f73f857d9264eb7af494c0fa0e6d8
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-01-18 01:59:07 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,5 +3,6 @@ end
 # code your #valid_move? method here
 
 def position_taken?(board, index)
+  board[index] != " " && 
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
