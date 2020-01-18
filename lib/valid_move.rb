e22a640991a412b9b5334b4dcafc87f219212504
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-01-18 02:26:16 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,6 +4,6 @@ end
 
 
 def position_taken?(board, index)
-  board[index] != " " &&  board[index] != "" && board[index] == "X" || board[index] == "O"
+  (board[index] != " ") &&  (board[index] != "") && (board[index] == "X") || (board[index] == "O")
 end
 
