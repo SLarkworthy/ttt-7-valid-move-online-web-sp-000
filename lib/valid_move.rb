afe85114ac5ccea21ad6840a2374c85dc96c1cfa
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-01-18 02:28:40 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,11 @@
+def position_taken?(board, index)
+  (board[index] != " ") && (board[index] != "") && (board[index] == "X") || (board[index] == "O")
+end
+
+
 def valid_move?(board, index)
   index.between?(0, 8) && !position_taken?
 end
 
 
-def position_taken?(board, index)
-  (board[index] != " ") && (board[index] != "") && (board[index] == "X") || (board[index] == "O")
-end
 
